e3f51dc73f23141394b37a3d5bc22e5f41f19f22
menachem554 <mendyrivkin@gmail.com>
2022-06-21 16:53:54 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <the-header></the-header>
+  <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+  provide() {
+    return {
+      isLoggedIn: this.isLoggedIn,
+      login: this.login,
+      logout: this.logout,
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/store/idex.js
@@ -0,0 +1,39 @@
+import { createStore } from 'vuex';
+
+import productsModules from './modules/products';
+import cartModules from './modules/cart';
+
+const store = createStore({
+  modules: {
+    prods: productsModules,
+    cart: cartModules,
+  },
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+  actions: {
+    login(context) {
+      context.commit('login');
+    },
+    logout(context) {
+      context.commit('logout');
+    },
+  },
+  getters: {
+    isAuthenticated(state) {
+      return state.isLoggedIn;
+    },
+  },
+});
+
+export default store;
